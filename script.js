bd8a5be312e39ebe67994a73330c3d35e0eedfa4
github_kpravina <github_pravinapravi15@gmail.com>
2021-10-03 13:00:27 +0530

    Expense Tracker

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+var balance = 0;
+var income = 0;
+var expense = 0;
+//localStorage.clear();
 
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+transaction=()=>{
+var incomeName = document.getElementById('text').value;
+var amount = parseInt(document.getElementById('amount').value);
+//localStorage.setItem(incomeName,amount)
+if(amount>0)
+{
+    income+=amount;
+    balance+=amount;
+}
+else
+{
+    balance+=amount;
+    expense-=amount;
+}
+document.getElementById('balance').innerHTML=' ₹'+balance;
+document.getElementById('money-plus').innerHTML=' ₹'+income;
+document.getElementById('money-minus').innerHTML=' ₹'+expense;
+var li = document.createElement('li');
+    var txt = document.createTextNode(incomeName);
+    li.appendChild(txt);
+    if(amount > 0){
+        li.className = 'plus'
+    }
+    else{
+        li.className = 'minus'
+    }
+    document.getElementById('list').appendChild(li);
+    var span = document.createElement("SPAN");
+    var amount = document.createTextNode(amount);
+    span.appendChild(amount);
+    li.appendChild(span);
+
+    document.getElementById("text").value = "";
+    document.getElementById("amount").value = "";
+}
